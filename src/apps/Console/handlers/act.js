64931dcf8a8729c89db7c15e1fabedb858395486
Ivan Herger <ivan.herger@gmail.com>
2021-01-24 15:40:57 +0100

    feat: registerAdapters

--- src/apps/Console/handlers/act.js
+++ src/apps/Console/handlers/act.js
@@ -1,18 +1,19 @@
-// import { parseMethodCall } from '../console-utils'
+import { parseMethodCall } from '../console-utils'
 import { encodeFunctionCallFromSignature } from '../web3-encoding-utils'
 
 export default async function actHandler(params, { wrapper }) {
   const [
     selectedAgentInstance,
     targetAddress,
-    // methodWithArgs,
+    methodWithArgs,
     ethAmount = '0',
   ] = params
-  // let [methodName, methodParams, methodArgs] = parseMethodCall(methodWithArgs)
+  let [methodName, methodParams, methodArgs] = parseMethodCall(methodWithArgs)
 
   // methodName = 'registerAsset'
   // methodName = 'addDerivatives'
-  let methodName = 'addAllowedMakers'
+  // methodName = 'addAllowedMakers'
+  methodName = 'registerAdapters'
 
   // methodParams = [
   //   'address',
@@ -24,7 +25,7 @@ export default async function actHandler(params, { wrapper }) {
   //   'bytes4[]',
   // ]
   // methodParams = ['address[]', 'address[]']
-  let methodParams = ['address[]']
+  methodParams = ['address[]']
 
   // methodArgs = [
   //   '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
@@ -49,10 +50,19 @@ export default async function actHandler(params, { wrapper }) {
   //   ['0x88D97d199b9ED37C29D846d00D443De980832a22'],
   //   ['0x9177a3354ee50bffbcc42c4c6bac27ed63979097'],
   // ]
-  let methodArgs = [
+  // methodArgs = [
+  //   [
+  //     '0x56178a0d5F301bAf6CF3e1Cd53d9863437345Bf9',
+  //     '0xe0238DA09Cab56B3066F26F98657DccE801c16B9',
+  //   ],
+  // ]
+  methodArgs = [
     [
-      '0x56178a0d5F301bAf6CF3e1Cd53d9863437345Bf9',
-      '0xe0238DA09Cab56B3066F26F98657DccE801c16B9',
+      '0x56a1892b2276bbb9968d1b5aa0000a71bf0fa7b8',
+      '0x4205073e7AD2F9896F827DBCe496dd2306DB602e',
+      '0xD050DC9E75f24Ae653d282D0cFb772871729e710',
+      '0x972318a0f4935c3153a2AA4c81274DfF621B360b',
+      '0x963E9C15a26d74085ABfC8B48280b650426F998e',
     ],
   ]
 
