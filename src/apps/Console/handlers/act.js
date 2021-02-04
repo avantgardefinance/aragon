defe41fbeb23cac38f82196523f4ec11cd068e63
Ivan Herger <ivan.herger@gmail.com>
2021-02-04 16:08:33 +0100

    feat: add ibETH and stETH

--- src/apps/Console/handlers/act.js
+++ src/apps/Console/handlers/act.js
@@ -10,9 +10,18 @@ export default async function actHandler(params, { wrapper }) {
   ] = params
   let [methodName, methodParams, methodArgs] = parseMethodCall(methodWithArgs)
 
-  methodParams = ['address[]']
+  methodParams = ['address[]', 'address[]']
 
-  methodArgs = [['0xed6Bdfa2725da6687bC89Bcb0BDB1e5dEAe15838']]
+  methodArgs = [
+    [
+      '0x67B66C99D3Eb37Fa76Aa3Ed1ff33E8e39F0b9c7A', // ibETH
+      '0xDFe66B14D37C77F4E9b180cEb433d1b164f0281D', // stETH
+    ],
+    [
+      '0x5f2E2E7FfbcAeE69d570D531A00228322632231E', // AlphaHomoraV1PriceFeed
+      '0xE2B19FA2D662F4eEA51D394b71Ce7281214840C5', // StakehoundETHPriceFeed
+    ],
+  ]
 
   const methodSignature = methodParams
     ? `${methodName}(${methodParams.join(',')})`
