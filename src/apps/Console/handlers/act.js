000575bcc294d0d8b7dc6cdf257f4cbf053ec516
Ivan Herger <ivan.herger@gmail.com>
2021-02-07 08:34:58 +0100

    feat: remove DPI

--- src/apps/Console/handlers/act.js
+++ src/apps/Console/handlers/act.js
@@ -10,16 +10,11 @@ export default async function actHandler(params, { wrapper }) {
   ] = params
   let [methodName, methodParams, methodArgs] = parseMethodCall(methodWithArgs)
 
-  methodParams = ['address[]', 'address[]']
+  methodParams = ['address[]']
 
   methodArgs = [
     [
-      '0x67B66C99D3Eb37Fa76Aa3Ed1ff33E8e39F0b9c7A', // ibETH
-      '0xDFe66B14D37C77F4E9b180cEb433d1b164f0281D', // stETH
-    ],
-    [
-      '0x5f2E2E7FfbcAeE69d570D531A00228322632231E', // AlphaHomoraV1PriceFeed
-      '0xE2B19FA2D662F4eEA51D394b71Ce7281214840C5', // StakehoundETHPriceFeed
+      '0x1494CA1F11D487c2bBe4543E90080AeBa4BA3C2b', // DPI
     ],
   ]
 
